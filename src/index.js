5b395780bfc88deb589d5c01d00e89497d2bd363
ssb00134 <59912657+ssb00134@users.noreply.github.com>
2021-03-14 17:23:10 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+import App from "./App";
+import rootReducer from "./modules";
+
+const store = createStore(rootReducer);
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
